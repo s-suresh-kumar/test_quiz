7e43ec27b51701b966d520953e31a8afa5c7d5aa
Suresh Kumar <suresh.uncbc.coding@gmail.com>
2023-03-29 07:11:12 -0400

    added  minimal  code to get the  timer to work

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 let startBtnEl =  document.getElementById("start-btn");
 startBtnEl.addEventListener("click", startTheQuiz);
 let startPageDivEl = document.getElementById("start-page-div");
+let timeLeft = 150;
+let timerId;
+let timeEl = document.getElementById("time");
 
 
 
 function startTheQuiz() {
 startPageDivEl.setAttribute("class", "hide");
 
+timerId = setInterval (OneSecTimerFunc,1000);
+timeEl.textContent =  timeLeft;
 }
+
+function OneSecTimerFunc() {
+  timeLeft -=1;  // timeLeft--;
+  timeEl.textContent =  timeLeft;
+
+}
+
