964d4ffafaf841fe8eac849ab9130d559182173a
Suresh Kumar <suresh.uncbc.coding@gmail.com>
2023-03-29 07:06:20 -0400

    added minimal code to hide start page on start  btn click

---
+++ script.js
@@ -0,0 +1,10 @@
+let startBtnEl =  document.getElementById("start-btn");
+startBtnEl.addEventListener("click", startTheQuiz);
+let startPageDivEl = document.getElementById("start-page-div");
+
+
+
+function startTheQuiz() {
+startPageDivEl.setAttribute("class", "hide");
+
+}
